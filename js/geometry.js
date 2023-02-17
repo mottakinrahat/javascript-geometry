d6d0e69069d5e1f5d82b28730e91334611809bd2
mottakinrahat <rahatchowhdury661@gmail.com>
2023-02-18 00:06:18 +0600

    functionJs

--- js/geometry.js
+++ js/geometry.js
@@ -1,4 +1,13 @@
 function getInputFromTextField(id){
  const takeValue=document.getElementById(id).value;
- return takeValue;
+ return parsefloat(takeValue);
+}
+function getElementFromElementField(id){
+    const takeValue=document.getElementById(id);
+    const takeValueInner=takeValue.innerText;
+    return parseFloat(takeValueInner);
+}
+function setTheTextValue(id,value){
+    const takeField=document.getElementById(id);
+    takeField.innerText=value;
 }
